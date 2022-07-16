2dfaa41e411f9d1de6b1839d886d62d6f49b389e
yevhen <yevhenfc@gmail.com>
2022-07-16 15:45:07 +0300

    isHelloMode

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import { Component, Fragment} from 'react';
 import './App.css';
-import UserCard from './Components/UserCard'; 
-
-const currentUser1 = {
-  fullName: 'John Fox',
-  email: 'john@mail.com',
-  tel: '+5-123-12345-34',
-  imgSrc: 'https://img.freepik.com/free-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=2000'
-}
-
-const currentUser2 = {
-  fullName: 'SDADAD',
-  email: 'asascom',
-  tel: '+5-',
-  imgSrc: 'https://img.freepik.com/free-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=2000'
-}
-
-const currentUser3 = {
-  fullName: 'zxczzv',
-  email: 'gdfgdfg',
-  tel: '-34',
-  imgSrc: 'https://img.freepik.com/free-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=2000'
-}
+import HelloClass from './Components/HelloClass'; 
 
 export default class App extends Component{
   constructor(props){
     super(props);
-    }
+    };
   
   render(){
     return(
       <>
-      <UserCard user={currentUser1} />
-      <UserCard user={currentUser2} />
-      <UserCard user={currentUser3} />
+        <HelloClass name={"Vasya"} surname={"Ivanov"} isHelloMode={true}/>
+        <HelloClass name={"Tom"} surname={"Fox"} isHelloMode={false}/>
       </>
     )
   }
--- src/Components/HelloClass/index.jsx
+++ src/Components/HelloClass/index.jsx
@@ -3,13 +3,33 @@ import React, { Component } from 'react'
 export default class HelloClass extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      isHello: true,
+      msg: '',
+    }
   }
+
+  changeMode = () => {
+    if(this.props.isHelloMode) {
+      this.setState ({
+        isHello: true,
+        msg: 'Hello'
+      });
+    }
+    else {
+      this.setState ({
+        isHello: false,
+        msg: 'Bye',
+      });
+    }
+  } 
+
   render() {
   const {name, surname} = this.props;
-
+  this.changeMode();  
     return (
       <>
-        <p>Hello, {name} {surname}</p>
+        <p>{this.state.msg}, {name} {surname}</p>
       </>
     )
   }
