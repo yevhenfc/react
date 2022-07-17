11e6b61604c4eb05e5dbb89a4f14f3d6923852da
yevhen <yevhenfc@gmail.com>
2022-07-17 22:41:20 +0300

    state: child -> parent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import { Component, Fragment} from 'react';
-import './App.css';
-import Card from './Components/Card';
+import React, { Component} from 'react';
 import User from './Components/User';
 
 export default class App extends Component{
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     user: {
-  //       name: "Jeka",
-  //       email: "3@#",
-  //       imgSrc: '#',
-  //     }
-  //   }  
-  // };
+  constructor(props) {
+    super(props)
   
-  render(){
-    return(
-      <>
-        <User />
-      </>
+    this.state = {
+       name: 'Tom',
+       surname: 'Fox',
+       age: 20,
+       isSelected: false,
+    }
+  }
+  
+  selectHandler = () => {
+    const {isSelected} = this.state;
+    this.setState = ({isSelected: !isSelected});
+  }
+
+  render() {
+    const {name, surname, age, isSelected} = this.state;
+    return (
+      <User 
+        name       = {name}
+        surname    = {surname}
+        age        = {age}
+        isSelected = {isSelected}
+        selectHandler = {this.selectHandler}
+      />
     )
   }
 }
--- src/Components/User/index.jsx
+++ src/Components/User/index.jsx
@@ -1,32 +1,20 @@
-import React, { Component } from 'react';
-import styles from './User.module.css';
+import React, { Component } from 'react'
+import styles from './User.module.css'
 
 export default class User extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       name: 'Tom',
-       surname: 'Fox',
-       age: 20,
-       isSelected: false,
-    }
-  }
-  
   select = () => {
-    const {isSelected} = this.state;
-    this.setState({isSelected: !isSelected});
-  }
-
+    this.props.selectHandler();
+  } 
+  
   render() {
-    const {name,surname,age,isSelected} = this.state;
-    const userSelectStyle = isSelected ? styles.selected : null;
+    const {name, surname, age, isSelected, selectHandler} = this.props;
+    const selectedStyle = {isSelected} ? styles.selected : null;
     return (
-      <article className={userSelectStyle} onClick = {this.select}>
+      <div className={selectedStyle} onClick={this.select}>
         <p>{name}, {surname}</p>
         <p>{age}</p>
-      </article>
+      </div>
+      
     )
   }
 }
-
