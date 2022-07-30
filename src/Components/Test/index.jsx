25e8c09d56580bd32ab5510b0eec541bbeb427d1
yevhen <yevhenfc@gmail.com>
2022-07-30 15:51:58 +0300

    viewport without resize

--- src/Components/Test/index.jsx
+++ src/Components/Test/index.jsx
@@ -3,15 +3,11 @@ import React, { useEffect, useState } from 'react'
 export default function Test() {
   const[w, setW] = useState(window.innerWidth);
   const[h, setH] = useState(window.innerHeight);
-  const [inlineStyle, setInlineStyle] = useState(null);
-
+ 
   useEffect( () => {
-    window.addEventListener('resize',() =>{
         setW(window.innerWidth);
         setH(window.innerHeight);
-    })
-  }
-  )
+  })
 
 return (
     <>
