22e63193d0aa18af1311a4a93e6d9e99e66c62fe
yevhen <yevhenfc@gmail.com>
2022-08-18 06:03:12 +0300

    fix router component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import {Link, Route, Routes, NavLink} from 'react-router-dom'
-import About from './Pages/About'
-import Home from './Pages/Home'
-import Products from './Pages/Products'
-import NotFound from './Pages/NotFound'
-import Header from   './Components/Header'
-import MainMenu from './Components/MainMenu'
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import NotFound from './Pages/NotFound';
+import MainMenu from './Components/MainMenu';
+import Header from './Components/Header';
 
 export default function App() {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path='/'        element={<MainMenu/>} />
-        <Route index element={<Home />} />
-        <Route path='about'     element={<About />} />
-        <Route path='products/*' element={<Products />} />
-        <Route path='*' element={<NotFound />} />
+          <Route path='/' element={<MainMenu />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='products/*' element={<Products />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
       </Routes>
     </>
-  )
+  ) 
 }
--- src/Components/MainMenu/index.js
+++ src/Components/MainMenu/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import {Outlet}  from 'react-router-dom'
-import Header from '../Header'
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Header from '../Header';
 
 export default function MainMenu() {
   return (
